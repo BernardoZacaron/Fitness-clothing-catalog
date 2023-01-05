d0391f04162e497c5f2ab394fab37e9227d66458
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-05 16:11:24 -0300

    Adding hover effects on clothes cards

---
+++ FitnessClothingCatalog/public_html/script/script1.js
@@ -0,0 +1,10 @@
+const card = document.getElementById("roupa1");
+
+var info = document.getElementsByClassName(Object.keys({card})[0]);
+
+function Mostrar(){
+    info.style.display='block';
+}
+
+
+document.getElementById("").addEventListener("mouseover", Mostrar);
