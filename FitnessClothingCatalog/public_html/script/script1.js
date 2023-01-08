993c295fafeba2bd966abc434bbda58dc7c87c64
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-08 01:07:14 -0300

    Sorting by category fully working

--- FitnessClothingCatalog/public_html/script/script1.js
+++ FitnessClothingCatalog/public_html/script/script1.js
@@ -2,15 +2,37 @@
 var itensTodos = document.getElementsByClassName("celula");
 var itensMasc = document.getElementsByClassName("item-masc");
 var itensFem = document.getElementsByClassName("item-fem");
-var itensLanc = document.getElementsByClassName("");
+var itensLanc = document.getElementsByClassName("item-lanc");
 
 function filtrarMasculino(){
     [].forEach.call(itensTodos, function (div) {
         div.style.display = 'none';
     });
+    
+    [].forEach.call(itensMasc, function (div) {
+        div.style.display = 'block';
+        div.style.marginBottom = '36%';
+    });
 }
 
+function filtrarFeminino(){
+    [].forEach.call(itensTodos, function (div) {
+        div.style.display = 'none';
+    });
+    
+    [].forEach.call(itensFem, function (div) {
+        div.style.display = 'block';
+        div.style.marginBottom = '36%';
+    });
+}
 
-function tornarInvisivel(div){
-    div.style.display = 'none';
+function filtrarLancamento(){
+    [].forEach.call(itensTodos, function (div) {
+        div.style.display = 'none';
+    });
+    
+    [].forEach.call(itensLanc, function (div) {
+        div.style.display = 'block';
+        div.style.marginBottom = '36%';
+    });
 }
